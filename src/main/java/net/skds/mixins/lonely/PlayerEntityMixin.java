607501f3154501202dc5cc8bd4b2f7d88c8c6c7c
Sasai-Kudasai-BM <68680203+Sasai-Kudasai-BM@users.noreply.github.com>
2021-09-06 22:24:32 +0700

    test a

--- src/main/java/net/skds/mixins/lonely/PlayerEntityMixin.java
+++ src/main/java/net/skds/mixins/lonely/PlayerEntityMixin.java
@@ -3,11 +3,11 @@ package net.skds.mixins.lonely;
 import com.mojang.authlib.GameProfile;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -27,26 +27,38 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 
 	@Shadow
 	private PlayerContainer container;
+	@Shadow
+	public PlayerInventory inventory;
 
 	@Inject(method = "<init>",at = @At(value = "TAIL"))
 	public void cont(World w, BlockPos pos, float yaw, GameProfile profile, CallbackInfo ci) {
-		this.container = new EPlayerContainer(this.inventory, !world.isRemote, (PlayerEntity) (Object) this);
+		PlayerEntity p = (PlayerEntity) (Object) this;
+		if (!p.abilities.isCreativeMode) {
+			this.container = new EPlayerContainer(this.inventory, !world.isRemote, p);
+			inventory = new EPlayerInventory(p);
+		}
 	}
 
 	protected PlayerEntityMixin(EntityType<? extends LivingEntity> type, World worldIn) {
 		super(type, worldIn);
 	}
 
-	@Shadow
-	public final PlayerInventory inventory = new EPlayerInventory((PlayerEntity) (Object) this);
 
-	@Overwrite
-	public ItemStack getItemStackFromSlot(EquipmentSlotType slotIn) {
-		return PlayerHooks.getItemStackFromSlot(slotIn, (PlayerEntity) (Object) this);
+	@Inject(method = "getItemStackFromSlot",at = @At("HEAD"),cancellable = true)
+	public void getItemStackFromSlot(EquipmentSlotType slotIn, CallbackInfoReturnable<ItemStack> ci) {
+		PlayerEntity p = (PlayerEntity) (Object) this;
+		if (p.abilities.isCreativeMode) {
+			return;
+		}
+		ci.setReturnValue(PlayerHooks.getItemStackFromSlot(slotIn, (PlayerEntity) (Object) this));
 	}
 
-	@Overwrite
-	public void setItemStackToSlot(EquipmentSlotType slotIn, ItemStack stack) {
+	@Inject(method = "setItemStackToSlot",at = @At("HEAD"),cancellable = true)
+	public void setItemStackToSlot(EquipmentSlotType slotIn, ItemStack stack, CallbackInfo ci) {
+		PlayerEntity p = (PlayerEntity) (Object) this;
+		if (p.abilities.isCreativeMode) {
+			return;
+		}
 		if (slotIn == EquipmentSlotType.MAINHAND) {
 			this.playEquipSound(stack);
 			PlayerHooks.setItemStackToSlot(slotIn, (PlayerEntity) (Object) this, stack);
@@ -57,6 +69,7 @@ public abstract class PlayerEntityMixin extends LivingEntity {
 			this.playEquipSound(stack);
 			PlayerHooks.setItemStackToSlot(slotIn, (PlayerEntity) (Object) this, stack);
 		}
+		ci.cancel();
 	}
 
 }
